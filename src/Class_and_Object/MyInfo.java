42550727c225b1664b8b2ae716d353437c48261a
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-04-04 12:39:13 +0530

    Create MyInfo.java

---
+++ src/Class_and_Object/MyInfo.java
@@ -0,0 +1,32 @@
+package Class_and_Object;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("------Personal Information------");
+		
+		String name = "Krushna Shahane";
+		char nameFirstChar = 'K';
+		byte age = 21;
+		int weight=76;
+		float height = 6.0f;
+		double CGPA = 6.47;
+		long mobNo = 7249429868l;
+		boolean isPassed = true;
+		
+		System.out.println("My name Is :- :" + name);
+		System.out.println("My Name First Character :- " + nameFirstChar);
+		System.out.println("My age is :- " + age);
+		System.out.println("My height is :- " + height);
+		System.out.println("My Weight Is :- " + weight);
+		System.out.println("My Mobile No IS :- " + mobNo);
+		System.out.println("I am Passed In Degree Or Not :- " + isPassed);
+		System.out.println("My CGPA :- " + CGPA);
+		
+		 
+		
+	}
+
+}
