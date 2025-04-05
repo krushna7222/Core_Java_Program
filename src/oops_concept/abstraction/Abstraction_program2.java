994923cfca18057251a92af15c18ee844a10ffd9
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-04-05 22:38:13 +0530

    Create Abstraction_program2.java

---
+++ src/oops_concept/abstraction/Abstraction_program2.java
@@ -0,0 +1,47 @@
+package oops_concept.abstraction;
+
+abstract class HemrajApp
+{
+	void Gallary()
+	{
+		System.out.println("Hemraj Created Gallary Features...");
+	}
+	
+	abstract void video();
+	abstract void audio();
+	abstract void callLog();	
+}
+
+abstract class KrushnaApp extends HemrajApp
+{
+	void audio()
+	{
+		System.out.println("Krushna Created Audio Features...");
+	}
+	void video()
+	{
+		System.out.println("Krushna Created Video Features...");
+	}
+}
+
+class Sachin extends KrushnaApp
+{
+	void callLog()
+	{
+		System.out.println("Sachin Created CallLog Features...");
+	}
+}
+
+public class Abstraction_program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sachin a1 = new Sachin();
+		a1.audio();
+		a1.Gallary();
+		a1.video();
+		a1.callLog();
+	}
+
+}
