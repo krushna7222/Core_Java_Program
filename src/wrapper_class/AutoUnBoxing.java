53beffe21ca326822981c9210f5fee7ecc145b05
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-04-29 23:16:25 +0530

    Create AutoUnBoxing.java

---
+++ src/wrapper_class/AutoUnBoxing.java
@@ -0,0 +1,30 @@
+package wrapper_class;
+
+//conversion of object into  primitive values using wrapper classes called as autoUnboxing.....
+public class AutoUnBoxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Integer i= 78;
+		 * 
+		 * System.out.println("Integer object = "+i); //78
+		 * 
+		 * // int u= i.intValue(); //unboxing int u= i ; //java5 //autounboxing
+		 * 
+		 * System.out.println("int primitive value  = "+u); //78
+		 */
+
+		/*
+		 * Character ii= 'j';
+		 * 
+		 * System.out.println("Character object = "+ii); //78
+		 * 
+		 * // char y= ii.charValue(); //unboxing char d = y ; //java5 //autounboxing
+		 * 
+		 * System.out.println("char primitive value  = "+d); //78
+		 */
+	}
+
+}
