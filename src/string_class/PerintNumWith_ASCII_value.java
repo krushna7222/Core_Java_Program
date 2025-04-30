71c5901f7ab6062e4d23111ab2999cb130d1ddcb
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-04-30 21:47:12 +0530

    Create PerintNumWith_ASCII_value.java

---
+++ src/string_class/PerintNumWith_ASCII_value.java
@@ -0,0 +1,19 @@
+package string_class;
+
+public class PerintNumWith_ASCII_value {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "..........";
+
+		// String ss=".";
+		char ch = 'A' / 'A'; // 65/65
+
+		// for(int i= ss.length() ; i<= s.length() * s.length(); i++) //without ASCII
+		// or
+		for (int i = ch; i <= 'd'; i++) // with ASCII
+		{
+			System.out.println(i);
+		}
+	}
+}
