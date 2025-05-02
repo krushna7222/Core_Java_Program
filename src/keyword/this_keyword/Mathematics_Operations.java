d12b6fef9b9e366f7f0acd4bc7a51c4ef716f6d2
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-05-03 00:18:37 +0530

    Create Mathematics_Operations.java

---
+++ src/keyword/this_keyword/Mathematics_Operations.java
@@ -0,0 +1,55 @@
+package keyword.this_keyword;
+
+
+class Operation
+{
+    int a,b;
+    void acceptValues(int a,int b)
+    {
+    	this.a=a;
+    	this.b=b;
+        
+    }
+    void add()
+    {
+    	int c=a+b;
+    	System.out.println("The Addition Of a+b Is :- " + c);
+    	square();
+        
+    }
+    void square()
+    {
+    	
+    	System.out.println("Square Of a Is :- "+ a*a );
+    	System.out.println("Square Of b Is :- " + b*b);
+        sub();
+    }
+    void sub()
+    {
+    	int d=a-b;
+    	System.out.println("Substraction Of a-b Is :- " + d);
+    	div();
+        
+    }
+    void div()
+    {
+    	int e=a/b;
+    	System.out.println("Substraction Of a/b Is :- " + e);
+    }
+}
+
+
+public class Mathematics_Operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Operation op1 = new Operation();
+		op1.acceptValues(20, 5);
+		
+        op1.add();
+      
+
+
+	}
+
+}
