c14fd19c3bb6b68d0eea1d911a16ba270c20c7be
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-05-09 22:43:55 +0530

    Create DownCasting.java

---
+++ src/type_casting/DownCasting.java
@@ -0,0 +1,42 @@
+package type_casting;
+
+public class DownCasting {
+	public static void main(String[] args) {
+
+		/*
+		 * //1. double(8) to byte(1)
+		 * 
+		 * double g= 35.6; System.out.println("double value  = "+g); //5.6
+		 * 
+		 * byte bt = (byte)g; //the syntax like "(byte)g" is called as cast operator=>
+		 * //syntax: (newDatatype)expression
+		 * 
+		 * System.out.println("byte value  = "+bt); //5
+		 */
+
+		/*
+		 * //2.int(4) to char(2)
+		 * 
+		 * int a= 122; System.out.println("int value  = "+a); //122
+		 * 
+		 * char y = (char)a;
+		 * 
+		 * System.out.println("char value  = "+y); // z
+		 * 
+		 */
+
+		/*
+		 * //3.int(4 byte) to boolean(1 bit)
+		 * 
+		 * int i=1; //1=true
+		 * 
+		 * System.out.println("int value  = "+i);
+		 * 
+		 * boolean j = (i==1) ? true : false ;
+		 * 
+		 * 
+		 * System.out.println("boolean value  = "+j);
+		 */
+
+	}
+}
