eb576eb14db9afc4f01ee591164f66c55ce1d744
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-09 23:02:58 +0530

    Create Fibonacci_String.java

---
+++ src/logical_program/Fibonacci_String.java
@@ -0,0 +1,21 @@
+package logical_program;
+
+public class Fibonacci_String {
+
+	public static void main(String[] args) {
+
+		String str = "nayan";
+		String reversed = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reversed = reversed + str.charAt(i);
+		}
+
+		if (str.equals(reversed)) {
+			System.out.println(str + " is a Palindrome.");
+		} else {
+			System.out.println(str + " is not a Palindrome.");
+		}
+	}
+
+}
